33c913ab7d403afbe5cfa18d3430f00311feabb2
Ange230700 <kouakouangeericstephane@gmail.com>
2024-05-31 08:27:58 +0200

    working...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,10 +5,58 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
 
+import ProtectedRoute from "./common/ProtectedRoute";
+
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Success from "./pages/Success";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/products/:category",
+        element: <ProductList />,
+      },
+      {
+        path: "/products/:id",
+        element: <Product />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+      {
+        path: "/login",
+        element: (
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/register",
+        element: (
+          <ProtectedRoute>
+            <Register />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
 ]);
 
