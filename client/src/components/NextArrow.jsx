2bcb84d325f82077f11caff3e68080a44d860947
Ange230700 <kouakouangeericstephane@gmail.com>
2024-06-03 10:54:12 +0200

    working...

--- client/src/components/NextArrow.jsx
+++ client/src/components/NextArrow.jsx
@@ -1,35 +1,19 @@
 import PropTypes from "prop-types";
 import { ArrowRightOutlined } from "@material-ui/icons";
 
-function NextArrow({ handleClick }) {
+function NextArrow(props = {}) {
+  const { onClick } = props;
+
   return (
     <button
       type="button"
       aria-label="next button"
       className="control-btn next"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
-        top: "50%",
-        right: "10px",
-        transform: "translateY(-50%)",
-        zIndex: 2,
-        background: "#fff7f7",
-        borderRadius: "50%",
-        width: "50px",
-        height: "50px",
-        cursor: "pointer",
-        opacity: 0.5,
-        border: "none",
-      }}
-      onClick={handleClick}
+      onClick={onClick}
     >
-      {" "}
-      <ArrowRightOutlined />{" "}
+      <ArrowRightOutlined />
     </button>
   );
 }
-NextArrow.propTypes = { handleClick: PropTypes.func };
+NextArrow.propTypes = { onClick: PropTypes.func };
 export default NextArrow;
--- client/src/components/PrevArrow.jsx
+++ client/src/components/PrevArrow.jsx
@@ -1,35 +1,19 @@
 import PropTypes from "prop-types";
 import { ArrowLeftOutlined } from "@material-ui/icons";
 
-function PrevArrow({ handleClick }) {
+function PrevArrow(props = {}) {
+  const { onClick } = props;
+
   return (
     <button
       type="button"
       aria-label="previous button"
       className="control-btn prev"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "absolute",
-        top: "50%",
-        left: "10px",
-        transform: "translateY(-50%)",
-        zIndex: 2,
-        background: "#fff7f7",
-        borderRadius: "50%",
-        width: "50px",
-        height: "50px",
-        cursor: "pointer",
-        opacity: 0.5,
-        border: "none",
-      }}
-      onClick={handleClick}
+      onClick={onClick}
     >
-      {" "}
-      <ArrowLeftOutlined />{" "}
+      <ArrowLeftOutlined />
     </button>
   );
 }
-PrevArrow.propTypes = { handleClick: PropTypes.func };
+PrevArrow.propTypes = { onClick: PropTypes.func };
 export default PrevArrow;
